9ea67f5e087fd5dc3832f6fc75870b80b7b777d5
Anand <anand.divakaran@intel.com>
2020-03-29 12:22:47 +0530

    Base Version

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+port = int(os.getenv('PORT'))
+
+
+@app.route('/')
+def hello-world():
+    return f"Hello World ! I am running on port {port}"
+
+
+if __name__ == '__main__':
+    app.run(port=port)
